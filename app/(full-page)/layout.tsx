9763b95ea006ae6f975da2bd9bf04cab29e6d02d
shengkai <maimemdada@gmail.com>
2024-02-23 09:32:00 +0700

    🎉 Init project

---
+++ app/(full-page)/layout.tsx
@@ -0,0 +1,21 @@
+import { Metadata } from 'next';
+import AppConfig from '../../layout/AppConfig';
+import React from 'react';
+
+interface SimpleLayoutProps {
+    children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+    title: 'Trổ Tài Dự Đoán',
+    description: 'Trang web dự đoán thể thao'
+};
+
+export default function SimpleLayout({ children }: SimpleLayoutProps) {
+    return (
+        <React.Fragment>
+            {children}
+            <AppConfig simple />
+        </React.Fragment>
+    );
+}
