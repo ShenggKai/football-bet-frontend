b639d67ba20ffe7cc5a6c3de0997c75e612cdbf9
shengkai <maimemdada@gmail.com>
2024-02-27 09:01:20 +0700

    Update axios base URL

--- lib/axiosConfig.tsx
+++ lib/axiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://e71f-137-59-44-39.ngrok-free.app',
+    baseURL: 'http://127.0.0.1:8000',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     timeout: 10000
 });
